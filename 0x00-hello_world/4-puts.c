c90887dc5424338f15b5b0459e193b8967eb9da7
Mercy Champ <mercykawich25@gmail.com>
2022-03-10 15:54:18 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-/*
+#include <stio.h>
+/**
  *main - entry point
- *Return: 0 after printing the function
+ *
+*Return: 0 after printing the function
  */
 int main(void)
 {
-puts("\"Programming is like building a puzzle);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
