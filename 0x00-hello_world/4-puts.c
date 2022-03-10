ac32da7e17707db7053e719e2fd6338967adb49b
Mercy Champ <mercykawich25@gmail.com>
2022-03-10 13:53:08 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: exactly Programming is like builing a multilingual puzzle using puts
+
+ */
+
+int  main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
