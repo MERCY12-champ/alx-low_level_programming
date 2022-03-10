717add6b9439980df5620a07d1d611ded6a741e5
Mercy Champ <mercykawich25@gmail.com>
2022-03-10 15:24:22 -0800

    Hello C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: exactly Programming is like builing a multilingual puzzle using puts
-
+/*
+ *main - entry point
+ *Return: 0 after printing the function
  */
-
-int  main(void)
-
+int main(void)
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a puzzle);
 return (0);
-
 }
