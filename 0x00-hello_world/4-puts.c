d9900b62f644c26aa432a6ff058a19ee5000ed8d
Mercy Champ <mercykawich25@gmail.com>
2022-03-11 04:37:11 -0800

    Yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
-[A[A
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: exactly Programming is like builing a multilingual puzzle using puts
+
+ */
+
+int  main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
