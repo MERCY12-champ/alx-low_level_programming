b449523a0db2afcd04dfbffb24728a9152930899
Mercy Champ <mercykawich25@gmail.com>
2022-03-11 05:50:31 -0800

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - Entry point
-
+ *main - entry point
  *
-
- * Return: exactly Programming is like builing a multilingual puzzle using puts
-
- */
-
-int  main(void)
-
+ *Return: 0 after printing the function
+ *
+int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
