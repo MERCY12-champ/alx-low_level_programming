1a3fe72c7c74ae9dec2debc8459e68b3fe979d94
Mercy Champ <mercykawich25@gmail.com>
2022-03-14 19:43:47 -0700

    Task 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,28 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- *main - Entry point
- *Return: Always 0 (Success)
- */
-int main(void)
-{
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
+#include <stdlib.h>                                                                                                                     
+
+#include <time.h>                                                                                                                       
+
+#include <stdio.h>                                                                                                                      
+
+/**                                                                                                                                     
+
+ *main - Entry point                                                                                                                    
+
+ *                                                                                                                                      
+
+ *Return: Always 0 (Success)                                                                                                            
+
+ */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+int n;                                                                                                                                  
+
+srand(time(0));                                                                                                                         
+
+n = rand() - RAND_MAX / 2;                                                                                                              
+
+if (n > 0)                                                                                                                              
+
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: exactly Programming is like builing a multilingual puzzle using puts
-
  */
-
 int  main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
