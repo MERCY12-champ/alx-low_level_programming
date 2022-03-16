b567c69a7e791d4b4fa816529d25f4cbe561d1d4
Mercy Champ <mercykawich25@gmail.com>
2022-03-15 19:43:23 -0700

    Main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,22 @@
-#include "main.h"
-/**
- * print_alphabet - print alphabet lowercase
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
  *
- * Return: Always 0.
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
  */
-void print_alphabet(void)
-{
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-_putchar('\n');
-}
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
