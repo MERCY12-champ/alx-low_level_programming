ae12f0a5d36b111b4459f96606e07feae7a02565
Mercy Champ <mercykawich25@gmail.com>
2022-03-15 18:48:55 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-/*
+*/
  * File: 0-putchar.c
  *
  */
@@ -10,14 +10,14 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }
