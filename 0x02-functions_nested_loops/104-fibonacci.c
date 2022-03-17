b05ec55386131eef6fc6a8b91adf31aaa1556730
Mercy Champ <mercykawich25@gmail.com>
2022-03-16 19:11:03 -0700

    Aprogram that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,4 @@
-/*
- * File: 104-fibonacci.c
- */
-#include <stdio.h
+#include <stdio.h>
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
  *        1 and 2, separated by a comma followed by a space.
