ba72854eb0d3446c50c1fbeb8c8739261e729ac5
Mercy Champ <mercykawich25@gmail.com>
2022-03-17 08:47:40 -0700

    Debugging 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,8 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
- *Return: 0
+ * Return: 0
  */
-int main(void)
-{
-int i;
 int main(void)
 {
 int i;
