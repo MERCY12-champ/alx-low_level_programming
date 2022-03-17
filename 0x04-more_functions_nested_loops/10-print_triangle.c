785d81dad15d9250fba1680da13d8e36adbd1165
Mercy Champ <mercykawich25@gmail.com>
2022-03-17 14:07:22 -0700

    A program that prints a triangle ,followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,34 +1,32 @@
 #include "main.h"
 /**
- * print_triangle - prints triangle
- * @size: print size
- * Return: Always 0.
+ *print_triangle - prints a triangle according to size
+ *
+ *@size:input
+ *
+ *Return: void
+ *
  */
 void print_triangle(int size)
 {
-int i;
-int z;
-int d;
-int p;
-if (size > 0)
+int i, j, gap;
+if (size <= 0)
 {
-d = size - 1;
-for (i = 0; i < size ; i++)
-{
-for (z = d; z > 0 ; z--)
-{
-_putchar (' ');
-}
-for (p = 0; p <= i; p++)
-{
-_putchar (35);
-}
-d--;
-_putchar ('\n');
-}
+_putchar('\n');
 }
 else
 {
-_putchar ('\n');
+for (i = 1; i <= size; i++)
+{
+for (gap = 1; gap <= (size - i); gap++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
 }
